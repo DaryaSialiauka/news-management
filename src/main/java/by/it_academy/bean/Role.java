5fa02e1bb17918674eb3298a6a0e17153effeec9
DaryaSialiauka <99286609+DaryaSialiauka@users.noreply.github.com>
2022-07-31 20:15:44 +0300

    Delete src/main directory

--- src/main/java/by/it_academy/bean/Role.java
+++
@@ -1,5 +0,0 @@
-package by.it_academy.bean;
-
-public enum Role {
-	ADMIN, USER;
-}
--- src/main/java/by/it_academy/controller/JSPPageName.java
+++
@@ -1,8 +0,0 @@
-package by.it_academy.controller;
-
-public class JSPPageName {
-	
-	public static final String BASE_PAGE = "/WEB-INF/jsp/base_page.jsp";
-	public static final String SING_IN_PAGE = "/WEB-INF/jsp/singin.jsp";
-	public static final String REGISTER_PAGE = "/WEB-INF/jsp/register.jsp";
-}
