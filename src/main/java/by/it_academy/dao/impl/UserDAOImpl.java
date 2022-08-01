54ee67ed8b92eda46fcbd4cb7fdf2e56df02d32b
DaryaSialiauka <99286609+DaryaSialiauka@users.noreply.github.com>
2022-08-01 20:16:49 +0300

    Add files via upload

--- src/main/java/by/it_academy/dao/impl/UserDAOImpl.java
+++ src/main/java/by/it_academy/dao/impl/UserDAOImpl.java
@@ -3,10 +3,8 @@ package by.it_academy.dao.impl;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Objects;
+
 
 import by.it_academy.bean.Role;
 import by.it_academy.bean.User;
@@ -25,8 +23,6 @@ public class UserDAOImpl implements UserDAO {
 
 	public boolean addUser(User user) throws AddUserDAOException {
 		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
 
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -34,41 +30,17 @@ public class UserDAOImpl implements UserDAO {
 			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/news?useSSL=false&serverTimezone=UTC", "root",
 					"7786405");
 
-			System.out.println("OK");
-
-			st = con.createStatement();
-
 			String sql = "INSERT INTO news.user(roles_id,login,password) VALUES(?,?,?)";
 			PreparedStatement ps = con.prepareStatement(sql);
 
-			ps.setInt(1, 1);
+			ps.setInt(1, 2);
 			ps.setString(2, user.getLogin());
 			ps.setString(3, user.getPassword());
 
 			ps.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-
-		} catch (ClassNotFoundException e1) { // TODO Auto-generated catch block
+		} catch (SQLException |ClassNotFoundException e1) { // TODO Auto-generated catch block
 			throw new AddUserDAOException("Not added user");
 		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-			try {
-				if (st != null) {
-					st.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
 			try {
 				if (con != null) {
 					con.close();
